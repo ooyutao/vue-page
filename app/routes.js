6c7ce53d867a2d3a6295035ee2f4f4bcac4ffeb9
yutao6 <yutao6@jd.com>
2018-08-08 14:41:52 +0800

    del

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,7 @@ var router = express.Router();
 /* 接口 */
 router.use('/api', require('./utils/api_util'));                     //京东稻田  AJAX接口转换
 
+router.get("/favicon.ico", function (req, res) {
+    res.redirect('./favicon.ico');
+});
 module.exports = router;
